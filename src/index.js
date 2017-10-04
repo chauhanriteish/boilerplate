ad3d54aaa7fc2e4dbb2f3df548f59eeeb85844d6
Ritesh <riteshchauhan_91@yahoo.com>
2017-10-04 11:59:55 -0400

    message

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () =>
+{
+    return<h1> Hi Ritesh from react! < /h1>
+
+}
+ReactDOM.render( < App / >, document.querySelector('#root'));
